3d43971d9cad3c99a226aee2970e802b8690e6c1
SaiKumar <29427150+saisgit@users.noreply.github.com>
2025-05-20 23:08:53 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -3,13 +3,15 @@ import pandas as pd
 import streamlit as st
 
 # Define Twitter handle and stock list
-twitter_handle = "REDBOXINDIA"
+#twitter_handle = "from:REDBOXINDIA"
 stock_list = ["DIXON", "DIVISLAB"]
 # earnings_dates = {"TSLA": "2025-05-15", "AAPL": "2025-05-20", "NVDA": "2025-05-25"}
 
 # Scrape tweets
 tweets = []
-for tweet in sntwitter.TwitterUserScraper(twitter_handle).get_items():
+query = "from:REDBOXINDIA"
+#for tweet in sntwitter.TwitterUserScraper(twitter_handle).get_items():
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     for stock in stock_list:
         if stock in tweet.content:
             #tweets.append([tweet.date, tweet.content, stock, earnings_dates.get(stock, "N/A")])
